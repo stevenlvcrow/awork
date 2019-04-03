2a763b63316619945c9f4835a92e3bd44428f758
stevenlvcrow@163.com <a13537578355>
2019-04-03 17:01:30 +0800

    文件夹层级结构修改

--- javaserver/src/java/com/miyou/Application.java
+++ javaserver/src/java/com/miyou/Application.java
@@ -4,12 +4,13 @@ package com.miyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @EntityScan("com.miyou")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
