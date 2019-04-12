8c66526965937e678880f975d599121eec59237f
stevenlvcrow@163.com <a13537578355>
2019-04-12 14:53:39 +0800

    python脚本提交

--- javaserver/src/java/com/miyou/utils/PythonUtils.java
+++ javaserver/src/java/com/miyou/utils/PythonUtils.java
@@ -1,9 +1,10 @@
 package com.miyou.utils;
 
-import org.python.core.Py;
-import org.python.core.PyFunction;
-import org.python.core.PyObject;
+import org.python.core.*;
 import org.python.util.PythonInterpreter;
+
+import java.io.*;
+import java.util.Properties;
 
 public class PythonUtils {
 
@@ -11,14 +12,33 @@ public class PythonUtils {
 
 
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Properties props = new Properties();
+        props.put("python.home", "path to the Lib folder");
+        props.put("python.console.encoding", "UTF-8");
+        props.put("python.security.respectJavaAccessibility", "false");
+        props.put("python.import.site", "false");
+        Properties preprops = System.getProperties();
+        PythonInterpreter.initialize(preprops, props, new String[0]);
+
+        PythonInterpreter interp = new PythonInterpreter();
+
         PathUtils pathUtils = new PathUtils();
-        PythonInterpreter pi1 = new PythonInterpreter();
-        pi1.execfile(pathUtils.getPythonPath()+"/add.py");
-        PyFunction pyf = pi1.get("add", PyFunction.class);
-        PyObject dddRes = pyf.__call__(Py.newInteger(2), Py.newInteger(3));
+        interp.exec("import sys");
+        interp.exec("sys.path.append('C:/Python27/Lib/site-packages')");//jython自己的
+        interp.exec("print(sys.path)");
+
+        InputStream is = new FileInputStream(new File("E:/awork/javaserver/src/resource/python/pl/xiaohuar.py"));
+        interp.execfile(is);
+        PyFunction pyFunction = interp.get("main",PyFunction.class);
+
+//        PyClass pyClass = interp.get("BookSpider",PyClass.class);
+        PyObject dddRes = pyFunction.__call__();
         System.out.println(dddRes);
-        pi1.cleanup();
-        pi1.close();
+        interp.cleanup();
+
+        interp.close();
+
     }
 }
