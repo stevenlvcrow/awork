00e5801da43adbc158fc1abdabd32eee453f6da0
stevenlvcrow@163.com <a13537578355>
2019-04-12 15:16:19 +0800

    python脚本提交

--- javaserver/src/java/com/miyou/controller/HelloController.java
+++ javaserver/src/java/com/miyou/controller/HelloController.java
@@ -2,9 +2,12 @@ package com.miyou.controller;
 
 import com.miyou.model.User;
 import com.miyou.service.TestServiceImpl;
+import com.miyou.utils.PythonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.FileNotFoundException;
 
 @Controller
 public class HelloController {
@@ -12,10 +15,17 @@ public class HelloController {
     @Autowired
     TestServiceImpl<User> testService;
 
+    @Autowired
+    PythonUtils pythonUtils;
 
     @RequestMapping("/")
     public String index() {
         testService.methodA(new User("张三"));
+        try {
+            pythonUtils.invokPython("xiaohuar.py");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         System.out.println("测试修改");
         return "/index.html";
     }
--- javaserver/src/java/com/miyou/utils/PythonUtils.java
+++ javaserver/src/java/com/miyou/utils/PythonUtils.java
@@ -1,35 +1,31 @@
 package com.miyou.utils;
 
+import lombok.extern.java.Log;
 import org.python.core.*;
 import org.python.util.PythonInterpreter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.*;
-import java.util.Properties;
 
+@Log
+@Component
 public class PythonUtils {
 
+    @Value("${python.home}")     private String pythonHome;
 
+    public void invokPython(String pyName) throws FileNotFoundException {
 
-
-
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Properties props = new Properties();
-        props.put("python.home", "path to the Lib folder");
-        props.put("python.console.encoding", "UTF-8");
-        props.put("python.security.respectJavaAccessibility", "false");
-        props.put("python.import.site", "false");
-        Properties preprops = System.getProperties();
-        PythonInterpreter.initialize(preprops, props, new String[0]);
 
         PythonInterpreter interp = new PythonInterpreter();
 
         PathUtils pathUtils = new PathUtils();
         interp.exec("import sys");
-        interp.exec("sys.path.append('C:/Python27/Lib/site-packages')");//jython自己的
+        interp.exec("sys.path.append('"+pythonHome+"')");//jython自己的
         interp.exec("print(sys.path)");
 
-        InputStream is = new FileInputStream(new File("E:/awork/javaserver/src/resource/python/pl/xiaohuar.py"));
+        InputStream is = new FileInputStream(new File(pathUtils.getPythonPath()+"/pl/"+pyName));
         interp.execfile(is);
         PyFunction pyFunction = interp.get("main",PyFunction.class);
 
@@ -37,8 +33,14 @@ public class PythonUtils {
         PyObject dddRes = pyFunction.__call__();
         System.out.println(dddRes);
         interp.cleanup();
-
         interp.close();
+    }
+
+
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
 
     }
 }
